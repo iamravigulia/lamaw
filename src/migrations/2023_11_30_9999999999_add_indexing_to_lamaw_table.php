c72e9c616a22a36f87653bf74973e4badc2f88c0
Ravi Gulia <ravigulia@bitbucket.org>
2021-11-11 11:43:48 +0530

    lamaw

---
+++ src/migrations/2023_11_30_9999999999_add_indexing_to_lamaw_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class addIndexingToLamawTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('fmt_lamaw_ans')) {
+            Schema::table('fmt_lamaw_ans', function (Blueprint $table) {
+                $table->index('question_id');
+                $table->index('active');
+                $table->index('arrange');
+                $table->index('media_id');
+            });
+        }
+        if (Schema::hasTable('fmt_lamaw_ques')) {
+            Schema::table('fmt_lamaw_ques', function (Blueprint $table) {
+                $table->index('media_id');
+                $table->index('active');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('fmt_lamaw_ques');
+    }
+}
